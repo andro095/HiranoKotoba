87fd3b1e66c0619cdeec68b064943e29a79340af
andro095 <andre.rodriguezgt@yahoo.com>
2023-03-30 01:30:08 -0600

    Base directory structure added

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+
+//theme
+import "./theme/theme.css";     
+    
+//core
+import "primereact/resources/primereact.min.css";
+
+//icons
+import "primeicons/primeicons.css";                                         
+        
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
